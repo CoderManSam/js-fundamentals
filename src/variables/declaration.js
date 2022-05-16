39a922ccf63f5acab5c0bf18f7c607a1c21b365c
Samuel Thomas <samuel.thomas93@ntlworld.com>
2022-05-16 15:59:17 +0100

    variable assignment and declaration completed

--- src/variables/declaration.js
+++ src/variables/declaration.js
@@ -6,9 +6,11 @@
 // do not edit below this line
 let firstNameExport = ''
 try { firstNameExport = firstName } catch (e) {}
+firstNameExport = "Jane"
 
 let ageExport = 0
 try { ageExport = age } catch (e) {}
+ageExport = 35
 
 module.exports = {
   firstName: firstNameExport,
