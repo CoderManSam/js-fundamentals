83e57c4651cbdee8955ff73b0816bddc58714800
Samuel Thomas <samuel.thomas93@ntlworld.com>
2022-05-18 16:21:08 +0100

    functions exercises completed

--- src/functions/creating-functions-multiple-args.js
+++ src/functions/creating-functions-multiple-args.js
@@ -9,6 +9,13 @@
 // -1, 1        | [-1, 0, 1]
 //
 // TODO: write code below
+function range(lower, upper) {
+  const numberRange = [];
+  for (let i = lower; i <= upper; i++) {
+      numberRange.push(i)
+  }
+  return numberRange
+}
 
 // define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
@@ -21,9 +28,12 @@
 // error, 10    | ERROR!!!!!!!!!!
 //
 // TODO: write code below
+function exclamation(string, num) {
+  return string.toUpperCase() + '!'.repeat(num)
+}
 
 // change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined // change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  a: range, // change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: exclamation // change undefined to be the name of the function defined to return the string with exclamations (the second todo)
 }
